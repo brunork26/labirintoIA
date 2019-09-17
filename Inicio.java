8fc25cd95c3b2b30ee5115a3021269bb6494b27c
Bruno Raupp Kieling <bruno@Brunos-MacBook-Pro.local>
2019-09-17 16:38:58 -0300

    Arquivo readme adicionado e script para fácil compilação

--- Inicio.java
+++ Inicio.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+
 public class Inicio {
-    public void main(String argv) {
-        
+    public static LeitorDeArquivo labirinto = new LeitorDeArquivo();;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor identifique o nome do labirinto que se encontra na pasta /labirintos/ :");
+        String nomeLabirinto = sc.nextLine();
+        labirinto.lerArquivo(nomeLabirinto);
     }
 }
--- LeitorDeArquivo.java
+++ LeitorDeArquivo.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LeitorDeArquivo {
-    
+
+    public static void lerArquivo(String labirinto) {
+        
+        System.out.printf("\nConteúdo do labirinto:\n");
+        try {
+            FileReader arquivo = new FileReader("./labirintos/" + labirinto + ".txt");
+            BufferedReader lerArquivo = new BufferedReader(arquivo);
+
+            String linha = lerArquivo.readLine();
+            while (linha != null) {
+                System.out.printf("%s\n", linha);
+                linha = lerArquivo.readLine(); // lê da segunda até a última linha
+            }
+
+            arquivo.close();
+        } catch (IOException e) {
+            System.err.printf("Problema ao abrir arquivo!\n", e.getMessage());
+        }
+        System.out.println();
+    }
+
 }
