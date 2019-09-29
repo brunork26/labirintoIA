a5d58f8caafc329ab95efc6d54186d99633c7459
Bruno Raupp Kieling <bruno@Brunos-MacBook-Pro.local>
2019-09-29 20:22:26 -0300

    main atualizada

--- Inicio.java
+++ Inicio.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 
 public class Inicio {
-    public static LeitorDeArquivo leLabirinto = new LeitorDeArquivo();;
+    public static LeitorDeArquivo leLabirinto = new LeitorDeArquivo();
+    public static Labirinto labirinto;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Por favor identifique o nome do labirinto que se encontra na pasta /labirintos/ :");
         String nomeLabirinto = sc.nextLine();
-        Labirinto labirinto = new Labirinto(labirinto.lerArquivo(nomeLabirinto));
-        
+        labirinto = new Labirinto(leLabirinto.lerArquivo(nomeLabirinto));
+        labirinto.imprimeLabirinto();
+        sc.close();
     }
 }
