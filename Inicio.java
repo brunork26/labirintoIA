6aff6adcc255422e0632d99fdc895ae7147c0dbf
Bruno Raupp Kieling <bruno@Brunos-MacBook-Pro.local>
2019-09-29 22:50:40 -0300

    Implementação do Astar

--- Inicio.java
+++ Inicio.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -11,6 +12,41 @@ public class Inicio {
         String nomeLabirinto = sc.nextLine();
         labirinto = new Labirinto(leLabirinto.lerArquivo(nomeLabirinto));
         labirinto.imprimeLabirinto();
+
+        
+        String[][] campo = labirinto.getCampo();
+            
+        int posicaoInicialI = 0;
+        int posicaoInicialJ = 0;
+        int posicaoFinalI = 0;
+        int posicaoFinalJ = 0;
+        int cont = 0;
+        int tamanho = labirinto.qtdBloqueios();
+        int[][] block = new int[tamanho][2];  
+        for(int i = 0; i < campo.length; i++) {
+            for(int j = 0; j < campo[i].length; j++) {
+                //System.out.println(campo[i][j]);
+                if(campo[i][j].equals("1")) {
+                    System.out.println("[" + i + ", " + j + "]");
+                    block[cont][0] = i;
+                    block[cont][1] = j;
+                    cont++;
+                } else if(campo[i][j].equals("E")) {
+                    posicaoInicialI = i;
+                    posicaoInicialJ = j;
+                } else if(campo[i][j].equals("S")) {
+                    posicaoFinalI = i; 
+                    posicaoFinalJ = j;
+                }
+            }
+        }
+        AStar astar = new AStar(campo.length, campo.length, posicaoInicialI, posicaoInicialJ, posicaoFinalI, posicaoFinalJ, 
+            block);
+        astar.display();
+        astar.processo(); // aplica o A*
+        // astar.displayPontos();
+        astar.displayResultado();
+
         sc.close();
     }
 }
